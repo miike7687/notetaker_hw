da476f5d46a88180e2f86587e37ed81dda6004ff
Michael Haber <michaeladam.haber@gmail.com>
2020-07-30 20:41:42 -0400

    Added more to server.js

--- develop/server.js
+++ develop/server.js
@@ -3,18 +3,54 @@ var express = require("express");
 var fs = require("fs");
 var app = express();
 var PORT = 4000;
+var path = require("path");
 
-app.get("/notes", function (req, res) {});
+// Middleware activation
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get("*");
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 // Get API notes - reading the JSON file and returning it in json format
 app.get("/api/notes", function (req, res) {
-  notesInfo = fs.readFileSync("/develop/db/db.json", "utf8");
+  notesInfo = fs.readFileSync("./develop/db/db.json", "utf8");
   console.log(notesInfo);
   notesInfo = JSON.parse(notesInfo);
   res.json(notesInfo);
 });
+
+app.post("/api/notes", function (req, res) {
+  var newNote = req.body;
+  console.log(newNote);
+  res.json(newNote);
+});
+
+//   try {
+
+//   //   notesInfo = fs.readFileSync("./develop/db/db.json", "utf8");
+//   //   notesInfo = JSON.parse(notesData);
+//   //   req.body.id = notesInfo.length;
+//   //   notesInfo.push(req.body);
+//   //   notesInfo = JSON.stringify(notesInfo);
+//   //   console.log(notesInfo);
+//   //   fs.writeFile("./develop/db/db.json", notesInfo, "utf8", function (err) {
+//   //     if (err) {
+//   //       console.log(err);
+//   //       throw err;
+//   //     }
+//   //   });
+//   //   res.json(JSON.parse(notesInfo));
+//   // } catch (err) {
+//   //   console.log(err);
+//   //   throw err;
+//   // }
+// });
 
 // Listen to the app and console log the url when the app is run
 app.listen(PORT, function () {
